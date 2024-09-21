c1b99206bf1e074b38fb1eb1102fab27fce84da5
ivanadimitrova123 <ivanadimitrova23@gmail.com>
2024-09-21 18:22:07 +0200

    added the filter for recipes by category

--- CheefsFeed-frontend/cheefsfeed-frontend/src/pages/Feed.js
+++ CheefsFeed-frontend/cheefsfeed-frontend/src/pages/Feed.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect, useContext } from "react";
-import axios from "../axios/axios"; // Updated import
+import axios from "../axios/axios";
 import Navbar from "../components/navbar";
 import { Store } from "../Store";
 import FeedItem from "../components/FeedItem";
+import { getHeaders } from "../utils";
 
 const Feed = () => {
   const { state } = useContext(Store);
   const { userInfo } = state;
   const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]); 
   const [isLoading, setIsLoading] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null); 
 
   useEffect(() => {
     const fetchRecipes = async () => {
       setIsLoading(true);
-      const headers = {
-        Authorization: `Bearer ${userInfo.token}`,
-      };
+      const headers = getHeaders(userInfo.token, false);
 
       try {
         const response = await axios.get("follow/recipes", { headers });
@@ -27,22 +28,73 @@ const Feed = () => {
       }
     };
 
-    fetchRecipes();
-  }, [userInfo]);
+    if (!selectedCategory) {
+      fetchRecipes();
+    }
+  }, [userInfo, selectedCategory]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("category/getAllCategories");
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  const handleCategoryClick = async (categoryId) => {
+    setIsLoading(true);
+    const headers = getHeaders(userInfo.token, false);
+
+    try {
+      const response = await axios.get(`follow/recipeByCategory?categoryId=${categoryId}`, { headers });
+      setRecipes(response.data);
+      setSelectedCategory(categoryId); 
+    } catch (error) {
+      console.error("Error fetching recipes by category:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className="container-fluid customBg" style={{ minHeight: "100vh" }}>
       <Navbar />
+      
+      {/* Display categories after the Navbar */}
+      <div className="row mb-3">
+        <div className="col">
+          <h3 className="mainHeader">Categories</h3>
+          <ul className="list-group">
+            {categories.map((category) => (
+              <li
+                key={category.id}
+                className={`list-group-item ${selectedCategory === category.id ? 'active' : ''}`}
+                onClick={() => handleCategoryClick(category.id)} 
+                style={{ cursor: "pointer" }}
+              >
+                {category.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
       <div className="row mb-3">
         <div className="col" style={{ display: "flex", alignItems: "center" }}>
           <h2
             className="mainHeader"
             style={{ width: "100%", textAlign: "center" }}
           >
-            Recipes Feed
+            {selectedCategory ? `Recipes for Category` : `Recipes Feed`}
           </h2>
         </div>
       </div>
+
       {isLoading && (
         <div className="text-center">
           <p>Loading...</p>
@@ -51,6 +103,7 @@ const Feed = () => {
           </div>
         </div>
       )}
+
       {recipes.length > 0 ? (
         <div className="row row-cols-1 row-cols-md-3 m-2 feedRow">
           {recipes.map((recipe) => (
@@ -62,7 +115,7 @@ const Feed = () => {
           ))}
         </div>
       ) : (
-        <p className="errorMessage">No recipes from followed users.</p>
+        <p className="errorMessage">No recipes found.</p>
       )}
     </div>
   );
--- CheefsFeed-frontend/cheefsfeed-frontend/src/pages/SavedRecipes.js
+++ CheefsFeed-frontend/cheefsfeed-frontend/src/pages/SavedRecipes.js
@@ -3,19 +3,21 @@ import axios from "../axios/axios";
 import Navbar from "../components/navbar";
 import { Store } from "../Store";
 import FeedItem from "../components/FeedItem";
+import { getHeaders } from "../utils";
 
 const SavedRecipes = () => {
   const { state } = useContext(Store);
   const { userInfo } = state;
   const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const fetchRecipes = async () => {
       setIsLoading(true);
-      const headers = {
-        Authorization: `Bearer ${userInfo.token}`,
-      };
+      const headers = getHeaders(userInfo.token, false);
 
       try {
         const response = await axios.get(
@@ -33,16 +35,64 @@ const SavedRecipes = () => {
     fetchRecipes();
   }, [userInfo]);
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const headers = getHeaders(userInfo.token, false);
+      try {
+        const response = await axios.get("category/getAllCategories", { headers });
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, [userInfo]);
+
+  const handleCategoryClick = async (categoryId) => {
+    setIsLoading(true);
+    const headers = getHeaders(userInfo.token, false);
+    setErrorMessage(''); 
+
+    try {
+      const response = await axios.get(`saverecipe/recipeByCategory?categoryId=${categoryId}`, { headers });
+      setRecipes(response.data);
+      setSelectedCategory(categoryId); 
+
+      if (response.data.length === 0) {
+        setErrorMessage("No recipes to show for this category.");
+      }
+    } catch (error) {
+      console.error("Error fetching recipes by category:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="container-fluid customBg" style={{ minHeight: "100vh" }}>
       <Navbar />
       <div className="row mb-3">
+        <div className="col">
+          <h3 className="mainHeader">Categories</h3>
+          <ul className="list-group">
+            {categories.map((category) => (
+              <li
+                key={category.id}
+                className={`list-group-item ${selectedCategory === category.id ? 'active' : ''}`}
+                onClick={() => handleCategoryClick(category.id)}
+                style={{ cursor: "pointer" }}
+              >
+                {category.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="row mb-3">
         <div className="col" style={{ display: "flex", alignItems: "center" }}>
-          <h2
-            className="mainHeader"
-            style={{ width: "100%", textAlign: "center" }}
-          >
-            Saved Recipes
+          <h2 className="mainHeader" style={{ width: "100%", textAlign: "center" }}>
+            {selectedCategory ? `Recipes for Category` : `Saved Recipes`}
           </h2>
         </div>
       </div>
@@ -54,6 +104,7 @@ const SavedRecipes = () => {
           </div>
         </div>
       )}
+      {errorMessage && <p className="errorMessage mt-4 fs-5">{errorMessage}</p>}
       {recipes.length > 0 ? (
         <div className="row row-cols-1 row-cols-md-3 m-2 feedRow">
           {recipes.map((recipe) => (
@@ -65,7 +116,7 @@ const SavedRecipes = () => {
           ))}
         </div>
       ) : (
-        <p className="errorMessage mt-4 fs-5">No recipes saved.</p>
+        selectedCategory && !errorMessage && <p className="errorMessage mt-4 fs-5">No recipes found.</p>
       )}
     </div>
   );
